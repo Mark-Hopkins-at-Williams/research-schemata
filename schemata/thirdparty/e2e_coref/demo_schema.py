594a247e2ae21f939cb25f853ff050090c4e4c3b
Mark Hopkins <hopkinsm@reed.edu>
2020-07-27 15:15:20 -0700

    Some changes to demo-schema.py.

--- schemata/thirdparty/e2e_coref/demo_schema.py
+++ schemata/thirdparty/e2e_coref/demo_schema.py
@@ -2,67 +2,24 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from six.moves import input
 import tensorflow as tf
-import coref_model as cm
-import util
+import schemata.thirdparty.e2e_coref.coref_model as cm
+from schemata.thirdparty.e2e_coref import util
 
 import nltk
 nltk.download("punkt")
-from nltk.tokenize import sent_tokenize, word_tokenize
 
-
-def create_example(text):
-  raw_sentences = sent_tokenize(text)
-  sentences = [word_tokenize(s) for s in raw_sentences]
-  speakers = [["" for _ in sentence] for sentence in sentences]
-  return {
-    "doc_key": "nw",
-    "clusters": [],
-    "sentences": sentences,
-    "speakers": speakers,
-  }
-
-def print_predictions(example):
-  words = util.flatten(example["sentences"])
-  for cluster in example["predicted_clusters"]:
-    print(u"Predicted cluster: {}".format([" ".join(words[m[0]:m[1]+1]) for m in cluster]))
-
-def make_predictions(text, model, session):
-  example = create_example(text)
-  tensorized_example = model.tensorize_example(example, is_training=False)
-  feed_dict = {i:t for i,t in zip(model.input_tensors, tensorized_example)}
-  _, _, _, mention_starts, mention_ends, antecedents, antecedent_scores, head_scores = session.run(model.predictions + [model.head_scores], feed_dict=feed_dict)
-
-  predicted_antecedents = model.get_predicted_antecedents(antecedents, antecedent_scores)
-
-  example["predicted_clusters"], _ = model.get_predicted_clusters(mention_starts, mention_ends, predicted_antecedents)
-  example["top_spans"] = zip((int(i) for i in mention_starts), (int(i) for i in mention_ends))
-  example["head_scores"] = head_scores.tolist()
-  return example
-  
-# New functions
-def format_predictions(example):
-    results = []
-    words = util.flatten(example["sentences"])
-    for cluster in example["predicted_clusters"]:
-      #print(cluster)
-      oneCluster = [" ".join(words[m[0]:m[1]+1]) for m in cluster]
-      results.append(oneCluster)
-    return results
-    
 def create_schema(verb, adj):
     return "they did not " + verb + " them because they were " + adj
 
-def generate_coref_input(passage):
-    speakers = []
-    for sent in passage:
-        speakers.append(['spk1' for x in sent])
-    return {"clusters": [], "doc_key": "nw", 
-            "sentences": passage, 
-            "speakers": speakers}
-
 def coref(model, passages):
+    def generate_coref_input(passage):
+        speakers = []
+        for sent in passage:
+            speakers.append(['spk1' for x in sent])
+        return {"clusters": [], "doc_key": "nw", 
+                "sentences": passage, 
+                "speakers": speakers}
     result = []
     with tf.Session() as session:
         model.restore(session)
@@ -90,6 +47,13 @@ def spans_from_coref(predictions):
     return result
 
 def they_or_them(model, verb_adj_pairs):
+    def format_predictions(example):
+        results = []
+        words = util.flatten(example["sentences"])
+        for cluster in example["predicted_clusters"]:
+          oneCluster = [" ".join(words[m[0]:m[1]+1]) for m in cluster]
+          results.append(oneCluster)
+        return results
     resultDict = {}
     for verb, adj in verb_adj_pairs:
         sentence = create_schema(verb, adj) # Create string of full sentence for prediction
--- test/test_coref.py
+++ test/test_coref.py
@@ -1,42 +1,60 @@
 import unittest
 
-from thirdparty.e2e_coref.demo_schema import *
+from schemata.thirdparty.e2e_coref.demo_schema import they_or_them , pairs_to_list
+from schemata.thirdparty.e2e_coref.demo_schema import coref, spans_from_coref
+
+import schemata.thirdparty.e2e_coref.coref_model as cm
+from schemata.thirdparty.e2e_coref import util
+
+
 
 class TestEvaluate(unittest.TestCase):
     def setUp(self):
         config = util.initialize_from_env('final')
-        model = cm.CorefModel(config)
+        self.model = cm.CorefModel(config)
     
-    def test_coref:
-        passages = [["they did not eat them because they were spoiled".split()]]
-        prediction = coref(model, passages)
-        assert prediction == [{'clusters': [], 'doc_key': 'nw', 'sentences': [['they', 'did', 'not', 'eat', 'them', 'because', 'they', 'were', 'spoiled']], 'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1']], 'predicted_clusters': [((4, 4), (6, 6))]}]
-
-        passages2 = [["they did not pick them because they were dirty".split()],["they did not purchase them because they were disgusted".split()]]
-        prediction = coref(model, passages2)
-        assert prediction == [
-            {'clusters': [], 'doc_key': 'nw', 'sentences': [['they', 'did', 'not', 'dick', 'them', 'because', 'they', 'were', 'dirty']], 'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1']], 'predicted_clusters': [((4, 4), (6, 6))]},
-            {'clusters': [], 'doc_key': 'nw', 'sentences': [['they', 'did', 'not', 'purchase', 'them', 'because', 'they', 'were', 'disgusted']], 'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1', 'spk1']], 'predicted_clusters': [((0, 0), (6, 6))]}
-            ]
+    def test_coref(self):
+        predictions = coref(self.model, [["i think therefore i am".split()], 
+                                         ["they think therefore they were".split()]])
+        assert len(predictions) == 2
+        assert predictions[0] == {'clusters': [],
+                                  'doc_key': 'nw',
+                                  'sentences': [['i', 'think', 'therefore', 'i', 'am']],
+                                  'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1']],
+                                  'predicted_clusters': [((0, 0), (3, 3))]}
+        assert predictions[1] == {'clusters': [],
+                                  'doc_key': 'nw',
+                                  'sentences': [['they', 'think', 'therefore', 'they', 'were']],
+                                  'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1']],
+                                  'predicted_clusters': [((0, 0), (4, 4))]}
+                
+    
+    def test_coref2(self):
+        predictions = coref(self.model, [["i think therefore i am".split()]])
+        assert len(predictions) == 1
+        assert predictions[0] == {'clusters': [],
+                                  'doc_key': 'nw',
+                                  'sentences': [['i', 'think', 'therefore', 'i', 'am']],
+                                  'speakers': [['spk1', 'spk1', 'spk1', 'spk1', 'spk1']],
+                                  'predicted_clusters': [((0, 0), (3, 3))]}
     
     def test_pairs_to_list(self):
         result = pairs_to_list("thirdparty/e2e_coref/schema.txt")
-        assertIn(('eat', 'spoiled'), result)
+        assert ('eat', 'spoiled') in result
         
     def test_spans_from_coref(self):
-        passages = [["they did not pick them because they were dirty".split()],["they did not eat them because they were disgusted".split()]]
-        clusters = spans_from_coref(coref(model, passages))
-        assert span[0] == {(4, 4), (6, 6)}
-        assert span[1] == {(0, 0), (6, 6)}
+        predictions = coref(self.model, [["i think therefore i am".split()], 
+                                         ["they think therefore they were".split()]])
+        clusters = spans_from_coref(coref(self.model, predictions))
+        assert clusters[0] == {(0, 0), (3, 3)}
+        assert clusters[1] == {(0, 0), (4, 4)}
         
     def test_they_or_them(self):
-        cluster = they_or_them(model, pairs_to_list("schema.txt"))
-        subjects = cluster["they"]
-        objects = cluster["them"]
-        assertIn(("pick", "disgusted"), subjects)
-        assertIn(("pick", "dirty"), objects)
-        assertIn(("purchase", "frugal"), subjects)
-        assertIn(("purchase", "unnecessary"), objects)
+        clusters = they_or_them(self.model, [('eat', 'spoiled'),('like', 'disappointed')])
+        subjects = clusters["they"]
+        objects = clusters["them"]
+        assert subjects == [('like', 'disappointed')]
+        assert objects == [('eat', 'spoiled')] 
         
 if __name__ == "__main__":
 	unittest.main()
