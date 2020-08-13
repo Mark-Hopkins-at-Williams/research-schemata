7f320d7fe986848dbc00122705234b37fb99b85e
Mark Hopkins <hopkinsm@reed.edu>
2020-08-13 14:45:29 -0700

    Added unit tests for spacy parser and refactored dependency parser methods.

--- schemata/parse/biaffine/biaffine.py
+++ schemata/parse/biaffine/biaffine.py
@@ -1,38 +1,9 @@
 from allennlp.predictors.predictor import Predictor
+from schemata.parse.util import DependencyParserWrapper
 
-def head_to_tree(heads):
-    # takes a list of dependency heads and returns each head's children as a dictionary
-    children = {}
-    for i in range(0,len(heads)+1):
-        children[i] = []
-    for j in range(0,len(heads)):
-        children[heads[j]].append(j+1)
-    return children
-            
-def descendents(tree,i):
-    # takes a dictionary and an index i and returns all descendents of the key at i
-    if tree[i] == []:
-        return [i]
-    else:
-        desc = [i]
-        for c in tree[i]:
-            desc+=(descendents(tree,c))
-        return desc
-    
-def get_spans(tree):
-    # takes a dictionary and returns a list of lists representing
-    # constituents
-    spans = []
-    for i in tree:
-        desc = descendents(tree,i)
-        # we ignore one-word constituents
-        if len(desc) > 1 and len(desc) < len(tree):
-            spans.append((min(desc)-1,max(desc)))
-    return spans
-          
-
-class BiaffineParser:
+class BiaffineParser(DependencyParserWrapper):
     def __init__(self):
+        super().__init__()
         self.parser = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz")
 
     def __call__(self, sent):
@@ -41,7 +12,7 @@ class BiaffineParser:
     def get_spans(self, sent):
         json_parse = self.parser.predict(sentence=sent)
         heads = json_parse['predicted_heads']
-        tree = head_to_tree(heads)
-        non_singletons = get_spans(tree)
+        tree = DependencyParserWrapper.head_to_tree(heads)
+        non_singletons = DependencyParserWrapper.compute_spans(tree)
         singletons = [(n,n+1) for n in range(len(heads))]
         return set(non_singletons) | set(singletons)
--- schemata/parse/spacy/spacy_parser.py
+++ schemata/parse/spacy/spacy_parser.py
@@ -1,4 +1,5 @@
 import spacy
+from schemata.parse.util import DependencyParserWrapper
 
 def get_tree_dict(doc):
     result = []
@@ -11,76 +12,23 @@ def get_tree_dict(doc):
           one_dict["governor"] = child.head.i + 1
           one_dict["dependent"] = child.i + 1
           result.append(one_dict)
-    return result
+    ls = sorted([(item['dependent'], item['governor']) for item in result])
+    ls = [x[1] for x in ls]
+    return ls
 
-def head_to_tree(dicts):
-    def fill_tree(one_dict, dicts):
-        # fills tree with missing keys
-        current = {}
-        for n in range(len(dicts) + 1):
-            if one_dict.get(n, None) == None:
-                current[n] = []
-            else:
-                current[n] = one_dict[n]
-        current[len(dicts)] = []  # create extra key:value pair
-        return current
-
-    # takes a list of dictionaries with heads and descendants and creates one dictionary with
-    # heads and descendants
-    tree_dict = {}
-    for one_dict in dicts:
-        head = one_dict["governor"]  # get  heads
-        child = one_dict["dependent"]  # get descendent of head
-        tree_dict[head] = tree_dict.get(head, []) + [child]
-
-    # fill tree with missing key/value pairs
-    tree_dict = fill_tree(tree_dict, dicts)
-    return tree_dict
-            
-def descendents(tree,i):
-    # takes a dictionary and an index i and returns all descendents of the key at i
-    if tree[i] == []:
-        return [i]
-    else:
-        desc = [i]
-        for c in tree[i]:
-            desc+=(descendents(tree,c))
-        return desc
-    
-def get_spans(tree):
-    # takes a dictionary and returns a list of lists representing
-    # constituents
-    spans = []
-    for i in tree:
-        desc = descendents(tree,i)
-        # we ignore one-word constituents
-        if len(desc) > 1 and len(desc) < len(tree):
-            spans.append((min(desc)-1,max(desc)))
-    return spans
-          
-
-class SpacyParser:
-    def __init__(self):
-        self.parser = spacy.load("en_core_web_lg")
+class SpacyParser(DependencyParserWrapper):
+    def __init__(self, model = "en_core_web_lg"):
+        super().__init__()
+        self.parser = spacy.load(model)
 
     def __call__(self, sent):
         return self.get_spans(sent)
         
     def get_spans(self, sent):
-        doc = self.parser(sentence=sent)
+        doc = self.parser(sent)
         heads = get_tree_dict(doc)
-        tree = head_to_tree(heads)
-        non_singletons = get_spans(tree)
+        tree = DependencyParserWrapper.head_to_tree(heads)
+        non_singletons = DependencyParserWrapper.compute_spans(tree)
         singletons = [(n, n+1) for n in range(len(heads))]
         return set(non_singletons) | set(singletons)
 
-#DEMO
-text = "The dog eats good food ."
-nlp = spacy.load("en_core_web_lg")
-doc = nlp(text)
-heads = get_tree_dict(doc)
-tree = head_to_tree(heads)
-non_singletons = get_spans(tree)
-print(non_singletons)
-singletons = [(n, n+1) for n in range(len(heads))]
-print(set(non_singletons) | set(singletons))
