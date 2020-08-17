83b06dbc1151e398cead5d779b8a47a5f0b2b418
Mark Hopkins <hopkinsm@reed.edu>
2020-08-17 14:19:56 -0700

    Removed duplicate installation of SuPar.

--- schemata/parse/supar/run_parser.py
+++ schemata/parse/supar/run_parser.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from schemata.parse.evaluate import evaluate, AttachmentSchema
-from schemata.parse.supar.supar_parser import suPar
+from schemata.parse.supar.supar_parser import SuPar
 import sys
 
 if __name__ == '__main__':
     file = sys.argv[1]
     schemas = AttachmentSchema.from_plaintext_file(file)
-    print(evaluate(schemas, suPar()))
+    print(evaluate(schemas, SuPar()))
--- test/test_supar_dep.py
+++ test/test_supar_dep.py
@@ -1,11 +1,11 @@
 import unittest
 
-from schemata.parse.supar_dep.supar_dep import suParDependency
+from schemata.parse.supar.supar_parser import SuParDependency
 
-class TestBiaffine(unittest.TestCase):
+class TestSuparDependency(unittest.TestCase):
     
     def test_get_spans(self):        
-        parser = suParDependency()
+        parser = SuParDependency()
         spans = parser("The dog eats good food .") 
         assert spans == {(0, 1), (0, 2), (0, 6), (1, 2), (2, 3),
                          (3, 4), (3, 5), (4, 5), (5, 6)}
