4f28ea3644740fcaf83d3dfe324c720827725b64
Mark Hopkins <hopkinsm@reed.edu>
2020-08-17 15:31:31 -0700

    Removed maltparser_test.py.

--- schemata/parse/maltparser/maltparser_test.py
+++
@@ -1,5 +0,0 @@
-import nltk
-from nltk.parse.malt import MaltParser
-mp = MaltParser('maltparser-1.9.2', 'engmalt.linear-1.7.mco')
-tree = mp.parse_one('The dog eats good food .'.split()).tree()
-print(tree)
