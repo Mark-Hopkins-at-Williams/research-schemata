318223b4c3491dd82c3bcbae090a468bc4c58e29
Julie Lely <julielely@gmail.com>
2020-08-20 12:08:38 -0700

    Updates for 8/20

--- schemata/parse/stanford/stanford.py
+++ schemata/parse/stanford/stanford.py
@@ -1,10 +1,6 @@
 from stanza.server import CoreNLPClient
 from schemata.parse.util import DependencyParserWrapper
 
-#IN TERMINAL:
-#export CORENLP_HOME=/path/to/stanford-corenlp-4.1.0
-#export CORENLP_HOME=/Users/hongly3j/reed-nlp/research-schemata/schemata/parse/stanford/stanford-corenlp-4.1.0
-
 def get_heads(dicts):
     # takes a list of dictionaries with heads and descendants and creates one dictionary with
     # heads and descendants
@@ -21,7 +17,6 @@ class StanfordParser(DependencyParserWrapper):
         return self.get_spans(sent)    
 
     def get_spans(self, sent):
-        #with CoreNLPClient() as client:
         ann = self.client.annotate(sent, annotators='parse', output_format='json')
         dependencies_list = ann['sentences'][0]['basicDependencies']
         heads = get_heads(dependencies_list)
